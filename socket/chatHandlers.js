380d2a01d5c3434e1f950793f52f55aad0621c0a
zohaib-softaims <zohaib@thesoftaims.coms>
2025-05-12 18:01:54 +0500

    predefine list of quesitons

--- socket/chatHandlers.js
+++ socket/chatHandlers.js
@@ -1,5 +1,7 @@
+import { predefinedQuesitons } from "../constants/predefinedQuestions.js";
 export const chatHandlers = (io, socket) => {
-  socket.on("sendMessage", async (message, callback) => {
-    console.log("message sent", message);
+  socket.on("sendMessage", async (data, callback) => {
+    const relevantQuestions = predefinedQuesitons[data.type];
+    console.log("message sent");
   });
 };
