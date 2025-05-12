fbe9c475d74a840f3aef21105a29eac1496464c4
zohaib-softaims <zohaib@thesoftaims.coms>
2025-05-12 19:19:18 +0500

    initial flow of llm response handling is defined

--- socket/chatHandlers.js
+++ socket/chatHandlers.js
@@ -1,7 +1,20 @@
 import { predefinedQuesitons } from "../constants/predefinedQuestions.js";
+import { getLLMResponse } from "../lib/llmConfig.js";
+import { generateLLMPrompt } from "../lib/llmPrompt.js";
+
 export const chatHandlers = (io, socket) => {
   socket.on("sendMessage", async (data, callback) => {
+    console.log("data is", data);
     const relevantQuestions = predefinedQuesitons[data.type];
-    console.log("message sent");
+    const systemPrompt = generateLLMPrompt(relevantQuestions, data.type);
+    const llmResponse = await getLLMResponse({
+      systemPrompt,
+      messages: data.messages,
+    });
+    console.log("llm response is", llmResponse);
+    socket.emit("receiveMessage", {
+      role: "assistant",
+      content: llmResponse,
+    });
   });
 };
